7236b52d9299c88a1a21c5b531b324034252d27d
Oleksandr Ronskyi <oleksandr.ronskyi@gmail.com>
2023-01-17 20:04:12 -0500

    component MovieDetails done

--- src/components/Movies/MovieDetails/MovieDetails.jsx
+++ src/components/Movies/MovieDetails/MovieDetails.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
 import { getMovieById } from "../../../api/moviesApi"
 
+import { MovieContainer } from "./MovieDetails.styled"
+
 export const MovieDetails = () => {
     const { movieId } = useParams()
     const [movieDetails, setMovieDetails] = useState()
@@ -9,31 +11,26 @@ export const MovieDetails = () => {
         getMovieById(movieId).then(setMovieDetails)
     }, [movieId])
 
-    console.log(movieDetails);
-
     if (!movieDetails) {
         return null
     }
 
     const { poster_path, overview, original_title, release_date, vote_average, genres } = movieDetails
-
     const yearOfRelease = release_date.slice(0, 4);
     const userScore = Math.round(vote_average * 10);
-    const listOfGenres = genres.map((genre) => genre.name).join(" ");
-
-    console.log(poster_path);
+    const listOfGenres = genres.map((genre) => genre.name).join(", ");
 
     return (
-        < div >
-            {<img src={poster_path} alt="" />}
+        < MovieContainer >
+            {<img src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt="" width={250} />}
             <div>
-                <h2>{original_title}({yearOfRelease})</h2>
+                <h2>{original_title} ({yearOfRelease})</h2>
                 <p>User Score: {userScore}%</p>
                 <h3>Overview</h3>
                 <p>{overview}</p>
                 <h3>Genres</h3>
                 <p>{listOfGenres}</p>
             </div>
-        </ div>
+        </ MovieContainer>
     )
 }
---
+++ src/components/Movies/MovieDetails/MovieDetails.styled.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const MovieContainer = styled.div`
+    display: flex;
+    gap: 40px;
+    margin-top: 30px;
+    padding-left: 60px;
+`
